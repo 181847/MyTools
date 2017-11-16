12dbd44bf6e383646710f27c5cd6e7826ad9320b
181847 <753090084@qq.com>
2017-11-16 13:25:36 +0800

    第一次上传文件

---
+++ MyAssert.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <exception>
+#include <string>
+class  SimpleException: std::exception
+{
+public:
+	const char* expr;
+	const char* file;
+	const int line;
+
+public:
+	SimpleException(const char *e, const char* f, const int line)
+		:expr(e), file(f), line(line)
+	{
+	}
+
+	inline std::string SimpleException::ToString()
+	{
+		char lineString[256];
+		std::string returnval;
+
+		sprintf_s(lineString, "\nFile:\t\t%s\nLine:\t\t%d\nExperssion:\t%s", file, line, expr);
+
+		returnval += lineString;
+		return returnval;
+	}
+};
+
+
+
+
+
+#if ASSERTIONS_ENABLED
+#define ASSERT(expr) \
+	if(expr){}\
+	else\
+	{\
+		throw SimpleException( #expr, __FILE__, __LINE__);\
+	}
+#else
+#define ASSERT(expr)//不求值
+#endif
+
+//这个宏适用于一些代码没有实现的时候，抛出这个异常，防止忘记一些代码的实现
+#define THROW_UNIMPLEMENT_EXCEPTION(pChar)\
+	{\
+		throw SimpleException(pChar, __FILE__, __LINE__);\
+	}
