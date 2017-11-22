e08b979fd2291b14fd8949d48312e6d1ae94d6bc
181847 <753090084@qq.com>
2017-11-22 21:04:52 +0800

    简化LuaTools的指针存储模块

--- LuaTools.h
+++ LuaTools.h
@@ -1,40 +1,33 @@
 #pragma once
 #include "DLLTools.h"
+#include "MyAssert.h"
+#include "MyTools.h"
+#include <functional>
 
 // this head file contain some tools for writing c module for lua
 
-// LuaPointerContainer is a struct that store the pointer which is created in 
-// the C/C++ enviroment, and the pointer do not need C/C++ to free or delete,
-// because the inner function 'cleaner' will be called when the struct is delete by the 
-// lua, and the 'cleaner' is defined as deleting the pointer itself.
-// However, the cleaner is public, and can be replaced by the lambda,
-// you can modify it whatever you want.
+// this is the container which store the pointer of the user.
 template<typename T>
 struct LuaPointerContainer
 {
+public:
 	T * pointer;
-	// using the cleaner to free the memeroy in the pointer.
-	std::function<void(void)> cleaner;
-	LuaPointerContainer()
-	{
-		cleaner = [&this]() {
-			delete this->pointer;
-			DEBUG_MESSAGE("Mesh Data has been cleaned.");
-		};
-	}
-	~LuaPointerContainer()
-	{
-		cleaner(pointer);
-	}
-
-	// reload '->'
-	T* operator->() const
-	{
-		return pointer;
-	}
 
 	void setPointer(T * p)
 	{
 		pointer = p;
+	}
+
+	T * operator -> () const
+	{
+		return pointer;
+	}
+
+	void freePointer()
+	{
+		if (pointer)
+		{
+			delete pointer;
+		}
 	}
 };
