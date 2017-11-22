915994dd748108c10b411888538567da4c326fcd
181847 <753090084@qq.com>
2017-11-22 21:06:58 +0800

    Revert "update"
    This reverts commit aa066769bbd6929d60feb5da4f92f6acf2517183.

--- MyTools.h
+++ MyTools.h
@@ -4,19 +4,14 @@
 #include <unordered_map>
 #include <memory>
 #include <fstream>
-#include <stdio.h>
-#include <functional>
 #include <DirectXMath.h>
 #include <DirectXPackedVector.h>
 #include "MyAssert.h"
+#include "Cleaner.h"
 
 #define Not(x) (!x)
-
-//分割字符串。
-void SplitString(const std::string& source, std::vector<std::string>& output, const std::string& splitBy);
 
 //删除类的复制构造函数和等于号重载函数。
 #define DELETE_COPY_CONSTRUCTOR(CLASS) \
 	CLASS(const CLASS&) = delete;\
-	CLASS& operator = (const CLASS&) = delete;
-
+	CLASS& operator = (const CLASS&) = delete
